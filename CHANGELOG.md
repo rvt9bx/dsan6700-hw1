e1772ac304810050ed8fb2ea850326d665afff1c
erinabruzzy <erinabrzusek@gmail.com>
2026-09-10 17:07:14 -0400

    cookiecutter with uv.lock and pyproject.toml

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v0.1.0 (10/09/2026)
+
+- First release of `mypkg`!
---
+++ src/mypkg/__init__.py
@@ -0,0 +1,3 @@
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("dsan6700-hw1")
